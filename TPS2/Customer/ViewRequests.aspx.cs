0ed71762fff27737ac2e9570ccf501894d2f3a62
Patrizha Cruz <47803186+Trizha1@users.noreply.github.com>
2019-08-15 20:09:55 -0700

    Update ViewRequests.aspx.cs

--- TPS2/Customer/ViewRequests.aspx.cs
+++ TPS2/Customer/ViewRequests.aspx.cs
@@ -1,14 +1,19 @@
-﻿using System;
+    
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Microsoft.AspNet.Identity;
+using TPS2.DBInteraction;
 
 namespace TPS2.Customer
 {
     public partial class ViewRequests : System.Web.UI.Page
     {
+        private DbConnect _dbConnect = new DbConnect();
+
         protected string SuccessMessage
         {
             get;
@@ -17,6 +22,31 @@ namespace TPS2.Customer
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                RequestDates.DataSource = _dbConnect.GetUsersRequests(User.Identity.GetUserId());
+                RequestDates.DataValueField = "Item2";
+                RequestDates.DataTextField = "Item1";
+                RequestDates.DataBind();
+            }
+        }
+
+        protected void RequestDates_OnSelectedIndexChanged(object sender, EventArgs e)
+        {
+            ClientRequest cr = _dbConnect.GetRequest(RequestDates.SelectedValue);
+            DetailsPanel.Visible = true;
+
+            //RequestedSkillListBox.
+            //RequiredSkillListBox
+            //EducationLevel.SelectedIndex = 
+            EducationRequired.Checked = cr.EducationRequired;
+            StartingSalary.Text = cr.Salary.ToString();
+            Address1TextBox.Text = cr.Location.AddressLine1;
+            Address2TextBox.Text = cr.Location.AddressLine2;
+            CityTextBox.Text = cr.Location.City;
+            //StatesListBox
+            ZipTextBox.Text = cr.Location.Zip;
+            //TelecommuteCheckBox.Checked = cr.
 
         }
     }
